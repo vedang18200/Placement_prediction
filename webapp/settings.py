55840a99ffb5e37f7b91be41d24aee93e7264553
Vedang Deshmukh <86526841+vedang18200@users.noreply.github.com>
2025-01-03 09:51:45 +0530

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y+ew@%vr^l8qr_isz_z@jgq8kxs%)&%^d=^o4#5#ef-w+il!i)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
